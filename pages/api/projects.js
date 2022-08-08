e7ab8f7ddfd2bbf6fc3125025f6e262c43322a27
Ahmed Mohammed <ahmed.mohd200130@gmail.com>
2022-08-08 16:34:35 +0200

    edit projects api

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,44 +1,44 @@
-import countries_image from "../../images/rest countries api.jpg";
-import ertugrul_image from "../../images/ertugrul.jpg";
-import furniture_image from "../../images/am-furniture.png";
-import quran_image from "../../images/quran Sound player.png";
-import quiz_image from "../../images/Quiz App.jpg";
-import github_image from "../../images/github users search.jpg";
 export const projects = [
     {
-      name: "Rest Countries Api",
-      img: countries_image,
-      demo_link:"https://a-mohammed2001.github.io/REST_Countries_Search_API/",
-      github_link:"https://github.com/a-mohammed2001/REST_Countries_Search_API",
+      name: "Qari BookStore",
+      img: '/images/Qari Bookstore.png',
+      demo_link:"https://qari-bookstore.vercel.app/",
+      github_link:"https://github.com/a-mohammed2001/Qari-Bookstore",
     },
     {
       name: "Ertugrul New Version Clone (React.js)",
-      img: ertugrul_image,
+      img: '/images/ertugrul.jpg',
       demo_link:"https://a-mohammed2001.github.io/Ertugrul_New_Version_Clone_React_Js/",
       github_link:"https://github.com/a-mohammed2001/Ertugrul_New_Version_Clone_React_Js",
+    }, 
+    {
+      name: "Rest Countries Api",
+      img: '/images/rest countries api.jpg',
+      demo_link:"https://rest-api-countries-search.vercel.app/",
+      github_link:"https://github.com/a-mohammed2001/REST_Countries_Search_API",
     },
     {
       name: "Am furniture",
-      img: furniture_image,
-      demo_link:"https://a-mohammed2001.github.io/Am_Furniture/",
+      img: '/images/am-furniture.png',
+      demo_link:"https://am-furniture.vercel.app/",
       github_link:"https://github.com/a-mohammed2001/Am_Furniture",
     },
     {
       name: "Quran Sound player",
-      img: quran_image,
-      demo_link:"https://a-mohammed2001.github.io/quran_Sound_player/",
+      img: '/images/quran Sound player.png',
+      demo_link:"https://quran-sound-player.vercel.app/",
       github_link:"https://github.com/a-mohammed2001/quran_Sound_player",
     },
     {
       name: "Quiz App",
-      img:quiz_image,
-      demo_link:"https://a-mohammed2001.github.io/Quiz_App/",
+      img: '/images/Quiz App.jpg',
+      demo_link:"https://quiz-app-delta-two.vercel.app/",
       github_link:"https://github.com/a-mohammed2001/Quiz_App",
     },
     {
       name: "Github Users search",
-      img: github_image,
-      demo_link:"https://a-mohammed2001.github.io/GitHub-users-search-app/",
+      img: '/images/github users search.jpg',
+      demo_link:"https://github-users-search-app.vercel.app/",
       github_link:"https://github.com/a-mohammed2001/GitHub-users-search-app",
     },
   ];
